281087690c2a512fe0db87a3a3853bf63bdd1a34
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-02-26 15:30:57 +0100

    add comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,46 @@ import './App.css'
 
 function App() {
   const [posts, setPosts] = useState([])
- 
-// fetch fa una chiamata al server tra partentesi 
-function featchPost(){ 
-  fetch("http://localhost:3000/posts/")
-// la prima concatenazione gestisce la richiesta e ci da una risposta che convenzionalmente viene nominata
-// =>response.json converte la risposta in json
-.then(response => response.json())
-.then(setPosts)
-}
- useEffect ( featchPost, [] )
+
+  //NOTE - impostaimo la funzioneche verrà richiamata da useEffect
+  function featchPost() {
+    // fetch fa una chiamata al server tra partentesi  in questo caso al nostro server node
+    fetch("http://localhost:3000/posts/")
+      // la prima concatenazione gestisce la richiesta e ci da una risposta che convenzionalmente viene nominata
+      // =>response.json converte la risposta in json
+      .then(response => response.json())
+      //NOTE - diamo a setPosts il valore della response
+      .then(setPosts)
+
+  }
+  //NOTE - passiamo la funzione della chiamata
+  useEffect(featchPost, [])
   return (
-    <> 
-    <table>
-     {posts.map((post)=>{
-      const {id,title,image,tags,content} = post
-      
-        return(
-          <tr key={id}>
-            <th><h1>{title}</h1></th>
-            <td><img className="table_image" src={`http://localhost:3000/public${image}`} alt="" /></td>
-            <td><p>{content}</p></td>
-            <td><p>{tags}</p></td>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <td>Ricetta</td>
+            <td>Immagine</td>
+            <td>Descrizione</td>
+            <td>Tag</td>
           </tr>
-        )
-     })}
-     </table>
+        </thead>
+        <tbody>
+          {posts.map((post) => {
+            //NOTE - destructuring object
+            const { id, title, image, tags, content } = post
+            return (
+              <tr key={id}>
+                <td><h1>{title}</h1></td>
+                <td><img className="table_image" src={`http://localhost:3000/public${image}`} alt="" /></td>
+                <td><p>{content}</p></td>
+                <td><p>{tags}</p></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </>
   )
 }
